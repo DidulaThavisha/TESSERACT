9fedc6fd2ed766461a8dd5f3bb523d68ab1699aa
Sasini Wanigathunga <92870335+SasiniWanigathunga@users.noreply.github.com>
2023-08-21 16:40:32 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,20 +6,19 @@ import torchvision
 
 class ResNet(nn.Module):
     """encoder + classifier"""
-    def __init__(self, name='resnet101', num_classes=2):
+    def __init__(self, name='resnet50', num_classes=2):
         super(ResNet, self).__init__()
-        if (name == 'resnet101'):
-            self.encoder = torchvision.models.resnet101(zero_init_residual=True)
+        if (name == 'resnet50'):
+            self.encoder = torchvision.models.resnet50(zero_init_residual=True)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(2048, num_classes)
         else:
-            self.encoder = torchvision.models.resnet50(zero_init_residual=True)
+            self.encoder = torchvision.models.resnet18(zero_init_residual=True)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(512, num_classes)
     def forward(self, x):
 
         return self.fc(self.encoder(x))
-
 
