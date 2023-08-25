c8fdbb5016f24df2c51e87ed4cb74518bee80b0d
Didula Samaraweera <samaraweeradt.20@uom.lk>
2023-08-25 23:58:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -14,8 +14,8 @@ class ResNet(nn.Module):
             self.encoder.classifier[6] = nn.Identity()
             self.fc = nn.Linear(4096, num_classes)
         elif name == 'unet':
-            self.encoder = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=num_classes, init_features=32, pretrained=True)
-            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=False, padding='SAME')
+            self.encoder = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=1, init_features=32, pretrained=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(1, 1), stride=(1, 1), padding=(1, 1))
             #self.encoder.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=False, padding=(1, 1))
             self.encoder.fc= nn.Identity()
             self.fc = nn.Linear(4096, num_classes)
